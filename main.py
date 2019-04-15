9ab7871d773280b3f36e17d720ca67e9c44dcb75
Aytac Ozkan <aytac.ozkn@gmail.com>
2019-04-15 22:24:54 +0200

    main.py is updated.

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-from impl.pso_impl import PSO
 from impl.ga_impl import GA
+from impl.pso_impl import PSO
 
 Nb_Cycles = 100
-Nb_Indiv  = 20
-polygon = ((10,10),(10,300),(250,300),(350,130),(200,10))
-w      = 0.9
+Nb_Indiv = 20
+polygon = ((10, 10), (10, 300), (250, 300), (350, 130), (200, 10))
+w = 0.9
 ro_max = 1
 
 pso = PSO(polygon=polygon, nb_Cycles=Nb_Cycles, nb_Indiv=Nb_Indiv, w=w, ro_max=ro_max)
 ga = GA(polygon=polygon, nb_Cycles=Nb_Cycles, nb_Indiv=Nb_Indiv, w=w, ro_max=ro_max)
 
 # PSO Algorithm
-#rgb(245,245,220)
-#rgb(220,20,60)
-print(pso.__exec__())
-print(pso.__isvalid__(polygon=polygon))
+# rgb(245,245,220)
+# rgb(220,20,60)
+# print(pso.__exec__())
+# print(pso.__isvalid__(polygon=polygon))
 # print(pso.__draw__(name="PSO",polygon=polygon, color=(255,250,250),clname="Blue"))
 # print(pso.__csv__(name="PSO"))
 
 # GA Algorithm
-#rgb(250,235,215)
-#rgb(0,191,255)
+# rgb(250,235,215)
+# rgb(0,191,255)
 # print(ga.__exec__())
 # print(ga.__draw__(name="GA",polygon=polygon,color=(255,250,250),clname="White"))
 # print(ga.__csv__(name="GA"))
-
