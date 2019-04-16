2ad7a83da90f65414a3679e6c54ee752ef12e955
Aytac Ozkan <aytac.ozkn@gmail.com>
2019-04-16 21:45:35 +0200

    readme.md is updated.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
+# Heuristic Stochastic optimization
 
-    pip3 freeze > 'requirements.txt'
+### Maximization of a building surface on a parcel of land. 
+    
+To able to run this application firstly you have to install 
+Python 3.7.0 version on your local machine or on docker container.     
+
+Then installation of dependencies on your python environment, you should follow as below:
+    
+    # pip3 freeze > 'requirements.txt'
     
     pip3 install -r requirements.txt
+    
+    python3.7 main.py
--- main.py
+++ main.py
@@ -10,18 +10,19 @@ ro_max = 1
 pso = PSO(polygon=polygon, nb_Cycles=Nb_Cycles, nb_Indiv=Nb_Indiv, w=w, ro_max=ro_max)
 
 # PSO Algorithm
-# rgb(245,245,220)#
+# Some rgb colors for changing the color of rectangle
+# rgb(245,245,220)
 # rgb(220,20,60)
-print(pso.__exec__())
-# print(pso.__isvalid__(polygon=polygon))
-print(pso.__draw__(name="PSO", polygon=polygon, color=(255, 250, 250), clname="Blue"))
+print(pso.__exec__()) # main execution function to run 'Particle swarm optimization' algorithm.
+print(pso.__draw__(name="PSO", polygon=polygon, color=(255, 250, 250), clname="Blue")) # Draw the rectangles on the polgon spaces which are given.
 print(pso.__csv__(name="PSO"))
 
 # GA Algorithm
+# Some rgb colors for changing the color of rectangle
 # rgb(250,235,215)
 # rgb(0,191,255)
-# ga = GA(polygon=polygon, nb_Cycles=Nb_Cycles, nb_Indiv=Nb_Indiv, w=w, ro_max=ro_max)
+ga = GA(polygon=polygon, nb_Cycles=Nb_Cycles, nb_Indiv=Nb_Indiv, w=w, ro_max=ro_max)
 
-# print(ga.__exec__())
-# print(ga.__draw__(name="GA", polygon=polygon, color=(255, 250, 250), clname="White"))
+# print(ga.__exec__()) # main execution function to run 'Genetic algorithm'
+# print(ga.__draw__(name="GA", polygon=polygon, color=(255, 250, 250), clname="White")) # Draw the rectangles on the polgon spaces which are given.
 # print(ga.__csv__(name="GA"))
